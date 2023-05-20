f04d64312c6bfdc7389a43905601ed99bb5ea956
seungyeon kim <rlatmddus159@naver.com>
2023-05-20 17:57:48 +0900

    로그도 생성

--- app/src/main/java/com/example/project3/LogIn.kt
+++ app/src/main/java/com/example/project3/LogIn.kt
@@ -2,6 +2,11 @@ package com.example.project3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
 //import android.util.Log
 //import android.widget.Toast
 //import kotlinx.android.synthetic.main.activity_log_in.sign_up
@@ -11,11 +16,24 @@ class LogIn : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
 
-//        sign_up.setOnClickListener {
-////            회원가입 누르면 해당 페이지로 넘어가는것
-//            Log.d("로그인화면 로그", "회원가입버튼눌림")
-//            Log.e("로그인화면 에러로그", "회원가입버튼 누를때 에러")
-//            Toast.makeText(this, "회원가입", Toast.LENGTH_SHORT).show()
-//        }
+        val id = findViewById<EditText>(R.id.id)
+        val pw = findViewById<EditText>(R.id.password)
+        val sign_up = findViewById<Button>(R.id.sign_up)
+        val log_in = findViewById<Button>(R.id.log_in)
+        log_in.setOnClickListener {
+            val servicenumber = id.text.toString()
+            val password = pw.text.toString()
+            if (servicenumber=="1810019" && password=="1234"){
+                Toast.makeText(this, "관리자입니다", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+        sign_up.setOnClickListener {
+//            회원가입 누르면 해당 페이지로 넘어가는것
+//            id가 1810019이면서 비밀번호가 1234 이면 관리자입니다 뜨게하기
+
+            Log.d("로그인화면 로그", "회원가입버튼눌림")
+
+        }
     }
 }
